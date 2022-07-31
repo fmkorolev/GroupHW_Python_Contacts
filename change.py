e3f018d5914594ed4950061634e70a6aa7377226
filip korolev <filippok089@gmail.com>
2022-07-31 12:00:09 +0400

    code is changed

--- change.py
+++ change.py
@@ -14,28 +14,30 @@ def add_contact():
     for each in data:
         data_str += each + ';'
     data_str += '\n'
-    with open('phonebook.txt', 'a') as file:
+    with open('phones.txt', 'a') as file:
         file.write(data_str)
 
 
 def write_data_dif_string(data):
-    with open('phonebook.txt', 'a') as file:
+    with open('phones.txt', 'a') as file:
         for each in data:
             file.write(each)
             file.write('\n')
 
 
 
-# def delete_contact(self):
-#         self.loadAll()
+def delete_contact(self):
+        self.loadAll()
 
-#         entry_to_delete = input("ENTER NAME OF ENTRY TO DELETE: ")
-#         if entry_to_delete in self.phonebook.keys():
-#             del self.phonebook[entry_to_delete]
-#             file = open(self.phonebook_file, 'w')
-#             for name, number in self.phonebook.items():
-#                 string = name + '\t' + number + '\n'
-#                 file.write(string)
-#             file.close()
-#             print("ENTRY DELETED SUCCESSFULLY")
+        entry_to_delete = input("Введите имя контакта: ")
+        if entry_to_delete in self.phonebook.keys():
+            del self.phonebook[entry_to_delete]
+            file = open(self.phones.txt, 'w')
+            for name, number in self.phonebook.items():
+                string = name + '\t' + number + '\n'
+                file.write(string)
+            file.close()
+            print("Контакт удален успешно")
+        else:
+            print("Контакт не найден")
 
